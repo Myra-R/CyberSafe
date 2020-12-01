8364450e20005cb7b702ff18b3c8c62c4227b894
Tanner Austin <tanner.a.austin@gmail.com>
2020-12-01 12:50:57 -0600

    server functionality

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 const controllers = require("./controllers")
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,9 +10,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use()
-app.use("/api/user", controllers.user)
-app.use("/api/userscore", controllers.userscore)
+// app.use()
+// app.use("/api/user", controllers.user)
+// app.use("/api/userscore", controllers.userscore)
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);

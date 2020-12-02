96f3fe6627679044fed55100efa178d1c39b7a07
Myra Rozmiarek <11marozmi@gmail.com>
2020-12-02 13:46:13 -0600

    updates to files

--- client/src/components/profile/index.js
+++ client/src/components/profile/index.js
@@ -1,48 +1,5 @@
 import React from "react";
 import './profile.css';
-<<<<<<< HEAD
-
-
-        function Profile() {
-        return (
-            <form id="profile">
-                <h1>Profile Section</h1>
-
-                <div id="form-firstName">
-                    <label>First name</label><br></br><br></br>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
-
-                <div id="form-lastName">
-                    <label>Last name</label><br></br><br></br>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
-
-                <div id="form-company">
-                    <label>What company are you applying with?</label><br></br><br></br>
-                    <input type="email" className="form-control" placeholder="Company Name" />
-                </div>
-
-                <div id="form-city">
-                    <label>What City were you born in?</label><br></br><br></br>
-                    <input type="password" className="form-control" placeholder="Enter City" />
-                </div>
-
-                <div id="form-question">
-                    <label>Other Random Questions....</label><br></br><br></br>
-                    <input type="password" className="form-control" placeholder="Random Answer" />
-                </div>
-
-
-
-              
-            </form>
-        );
-    
-
-}
-
-=======
 import Popup from '../popup'
 
 
@@ -83,5 +40,4 @@ function Profile() {
         </>
     );
 }
->>>>>>> 1a6576cc71b20dc0035ef4832566e5347bbc6e32
 export default Profile; 

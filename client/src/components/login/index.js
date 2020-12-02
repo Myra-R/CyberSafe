812a207c35adae0f6921d322131280836295713a
Isaac Arnold <isaac@iaivphotography.com>
2020-12-02 11:37:46 -0600

    javascript bootstrap functonality added, terms and conditions dropdown added

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -2,33 +2,40 @@ import React from "react";
 import './login.css';
 
 
-        function Login() {
-        return (
-            <form className="form">
-                <h3>Login</h3>
+function Login() {
+    return (
 
-                <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+        <form className="form">
+            <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Terms and Conditions
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <p>All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  All work and no play makes Jack a dull boy.  </p>
                 </div>
+            </div>
 
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
+            <div className="form-group">
+                <label>First name</label>
+                <input type="text" className="form-control" placeholder="First name" />
+            </div>
 
-                <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+            <div className="form-group">
+                <label>Last name</label>
+                <input type="text" className="form-control" placeholder="Last name" />
+            </div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
+            <div className="form-group">
+                <label>Email</label>
+                <input type="email" className="form-control" placeholder="Enter email" />
+            </div>
 
-              
-            </form>
-        );
-    }
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Enter password" />
+            </div>
+
+        </form>
+    );
+}
 export default Login; 

93857f6559e4f0481f008f83625cbadd725d8c1c
Myra Rozmiarek <11marozmi@gmail.com>
2020-12-02 11:05:24 -0600

    css animation

--- client/src/components/profile/index.js
+++ client/src/components/profile/index.js
@@ -4,31 +4,31 @@ import './profile.css';
 
         function Profile() {
         return (
-            <form className="form">
-                <h3>Profile Section</h3>
+            <form id="profile">
+                <h1>Profile Section</h1>
 
-                <div className="form-group">
-                    <label>First name</label>
+                <div id="form-firstName">
+                    <label>First name</label><br></br><br></br>
                     <input type="text" className="form-control" placeholder="First name" />
                 </div>
 
-                <div className="form-group">
-                    <label>Last name</label>
+                <div id="form-lastName">
+                    <label>Last name</label><br></br><br></br>
                     <input type="text" className="form-control" placeholder="Last name" />
                 </div>
 
-                <div className="form-group">
-                    <label>What company are you applying with?</label>
+                <div id="form-company">
+                    <label>What company are you applying with?</label><br></br><br></br>
                     <input type="email" className="form-control" placeholder="Company Name" />
                 </div>
 
-                <div className="form-group">
-                    <label>What City were you born in?</label>
+                <div id="form-city">
+                    <label>What City were you born in?</label><br></br><br></br>
                     <input type="password" className="form-control" placeholder="Enter City" />
                 </div>
 
-                <div className="form-group">
-                    <label>Other Random Questions....</label>
+                <div id="form-question">
+                    <label>Other Random Questions....</label><br></br><br></br>
                     <input type="password" className="form-control" placeholder="Random Answer" />
                 </div>
 

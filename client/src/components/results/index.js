ee9853150a20fbe6d9368dca25b7063ff77948ef
Isaac Arnold <isaac@iaivphotography.com>
2020-12-04 11:54:00 -0600

    jquery installed, color coding added but not yet functional

--- client/src/components/results/index.js
+++ client/src/components/results/index.js
@@ -1,23 +1,43 @@
 import React from "react";
 import FooterPage from "../footer";
 import './results.css';
+import $ from "jquery";
+
+
 // import { score, positives, negatives } from "../login/index";
 var score = 50;
 var positives = ["y", "e", "s"];
 var negatives = ["n", "o"];
 
 function Results() {
+    let scoreColor = $("<span class= 'at-risk'>");
     const posItems = positives.map((pos) =>
     <li>{pos}</li>);
     const negItems = negatives.map((neg) =>
     <li>{neg}</li>);
+    
+//color coding results
+    if (score < 100) {
+        scoreColor = $("<span class= 'at-risk'>");
+    }
+    else if ({score} < 200) {
+        scoreColor = $("<span class= 'adequate'>");
+    }
+    else {
+        scoreColor = $("<span class= 'safe'>");
+    };
+
+
+    scoreColor.text({score});
+    $(".test-results").append(scoreColor);
+
 
     return (
         <div>
             <form className="form">
                 <h1>Results</h1>
                 <br />
-                <h2 class="test-results">You Scored {score}</h2>
+                <span class="test-results">You Scored {score}</span>
 
 
                 <div className="form-group password-test">

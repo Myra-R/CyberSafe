cd0d4f261b03202a502a6450465554a3a21af593
Wendy Grant <wendykgrant.tx@gmail.com>
2020-12-04 14:36:54 -0600

    completed popup component

--- client/src/components/popup/footer.js
+++ client/src/components/popup/footer.js
@@ -7,31 +7,33 @@ const defaultFooterStyles = {
   overflow: 'hidden'
 }
 const closeBtnStyle = {
-  float:'left',
-  color: '#fff',
-  backgroundColor: '#DC143C',
-  lineHeight: '30px',
-  borderRadius: '3px',
-  border: '2px solid #ff4742',
-  fontSize: '13px',
-  fontWeight: 'bold'
-}
-const submitBtnStyle = {
   float:'right',
   color: '#fff',
-  backgroundColor: '#DC143C',
+  backgroundColor: 'red',
   lineHeight: '30px',
   borderRadius: '3px',
-  border: '2px solid #ff4742',
-  fontSize: '13px',
-  fontWeight: 'bold'
+  border: '2px solid #a10b29',
+  fontSize: '15px',
+  fontWeight: '600',
+  paddingLeft: '7px',
+  paddingRight: '7px' 
 }
+// const submitBtnStyle = {
+//   float:'right',
+//   color: '#fff',
+//   backgroundColor: '#DC143C',
+//   lineHeight: '30px',
+//   borderRadius: '3px',
+//   border: '2px solid #ff4742',
+//   fontSize: '13px',
+//   fontWeight: 'bold'
+// }
 
 const Footer = ({handleSubmit, footerStyles, handleClose}) => (
   <div style={footerStyles}>
     <div style={{padding:'10px'}}>
-    <button onClick={handleSubmit} style={submitBtnStyle} type='button'>Submit</button>
-    <button onClick={handleClose} style={closeBtnStyle} type='button'>Close</button>
+    {/* <button onClick={handleSubmit} style={submitBtnStyle} type='button'>Submit</button> */}
+    <button onClick={handleClose} style={closeBtnStyle} type='button'>Fix now</button>
     </div>
   </div>
 )

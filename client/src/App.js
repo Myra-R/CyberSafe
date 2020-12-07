8975d7903b03ba8bb89d372d7ac348761f6e6072
Isaac Arnold <isaac@iaivphotography.com>
2020-12-07 10:46:12 -0600

    404 page styled and routed

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,23 @@ import Login from './components/login'
 import Signup from './components/signup'
 import Profile from './components/profile'
 import Results from './components/results'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import FooterPage from './components/footer'
-
+import ourError from "./components/Error/sorry"
 function App() {
+
+  
   return (
     <Router>
-   
+      <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/results" component={Results} />
+        <Route component={ourError} />
     
     <FooterPage></FooterPage>
+    </Switch>
     </Router>
   );
 }
--- client/src/components/Error/sorry.js
+++ client/src/components/Error/sorry.js
@@ -1,14 +1,18 @@
 import React from "react";
+import errorImage from "./404-image.jpg"
 
 function OurError() {
     return (
         <div>
-            <h2>
-                404 Error
-            </h2>
-            <p>
+            <p class= "container">
                 We apologize for this, something appears to have broken. We are working with the gremlins to correct this error.
             </p>
+            <figure className="container col-md-4">
+                <img src= {errorImage} className="img-thumbnail" alt="404" />
+            </figure>
+            <footer>
+            <a href='https://www.freepik.com/vectors/background'>Background vector created by freepik - www.freepik.com</a>
+            </footer>
         </div>
     )
 }
